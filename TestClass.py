1c1635b9ee3785d90ac276a1fe9752720c45d078
Lucas Serrenho Pfefferkorn <43182111+Bill-Portugues@users.noreply.github.com>
2019-01-01 11:13:13 -0300

    Update TestClass.py

--- TestClass.py
+++ TestClass.py
@@ -85,26 +85,3 @@ class Test():
         assert self.TestCase.ChargingPrice() == total, "ChargingPrice is giving the wrong result"
 
         self.reset()
-
-
-#Set of different Samples to check all important aspects of the object under study
-
-TestSamples1=[["Weeks",4],["Hours",5]] # 2 Rental Units
-TestSamples2=[["Hours",1],["Hours",0],["Hours",2]] # 3 Rental Units
-TestSamples3=[["Days",9],["Days",5],["Days",14],["Weeks",3],["Days",0]] # 5 Rental Units
-TestSamples4=[["Weeks",4],["Hours",5],["Days",8],["Weeks",3],["Days",0],["Weeks",33],["Weeks",9]] # 7 Rental Units
-
-TestSamples=[TestSamples1,TestSamples2,TestSamples3,TestSamples4]
-
-
-TestCase=Test()                 #initializing object under study
-TestCase.InitializationTest()
-
-for samples in TestSamples:
-
-    TestCase.AddNewRentalTest(samples)
-    TestCase.RentalUnitsIntegrityTest(samples)
-    TestCase.OverallSumRentalsTest(samples)
-    TestCase.ChargingPriceTest(samples)
-
-print ("\n All Test were executed properly")
